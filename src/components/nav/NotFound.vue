3653221d81cbe430f5fa8a49b6cb2c727a3ab562
kibroman <you@example.comkibromanx@gmail.com>
2021-08-16 22:04:14 -0700

    updated routing

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+    <h2>Page Not Found. Maybe Try <router-link to="/teams">Teams</router-link></h2>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,42 @@
-import {createApp} from 'vue';
-import {createRouter,createWebHistory} from 'vue-router';
+import {
+    createApp
+} from 'vue';
+import {
+    createRouter,
+    createWebHistory
+} from 'vue-router';
 
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: [                                                                                                                       
-         { path: '/teams', component: TeamsList },
-         { path: '/users', component: UsersList },
-         { path: '/teams/:teamId', component: TeamMembers, props: true },
-         
+    routes: [{
+            path: '/',
+            redirect: '/teams'
+        },
+        {
+            path: '/teams',
+            component: TeamsList,
+            children: [{
+                path: ':teamId',
+                component: TeamMembers,
+                props: true
+            }]
+        },
+        {
+            path: '/users',
+            component: UsersList
+        },
+
+        {
+            path: '/:notFound(.*)',
+            component: NotFound
+        },
+
     ],
     linkActiveClass: 'active'
 });
